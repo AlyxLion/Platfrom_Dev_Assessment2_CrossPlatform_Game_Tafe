e688bac3a83961f92c1bfd78099abd4ac40e5b21
AlyxLion <MagentaLionAlyx@gmail.com>
2022-06-18 13:15:27 +1000

    Added project items
    Making sure I get it done befor sunday so James can make it

---
+++ Assets/RunTime/Cross-Platform/Scripts/CrossPlatformInput.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace ItJustWorks.CrossPlatform
+{
+	public class CrossPlatformInput : Singleton<CrossPlatformInput>
+	{
+		[RuntimeInitializeOnLoadMethod]
+		public static void OnRuntimeInitialise()
+		{
+			if(!IsValid() && FindObjectOfType<CrossPlatformInput>() == null)
+			{
+				GameObject newInput = Instantiate(Resources.Load<CrossPlatformInput>("Cross-Platform Input")).gameObject;
+				newInput.name = newInput.name.Replace("(Clone)", "");
+				DontDestroyOnLoad(newInput);
+			}
+		}
+
+		public static Vector2 GetMovementAxis() => Instance.joystick != null ? Instance.joystick.Axis : Vector2.zero;
+
+		[SerializeField] private PlayerInput input;
+		[SerializeField] private MobileJoystickHandler joystick;
+		[SerializeField] private SwipeInputHandler swipeInput;
+
+		private void Start()
+		{
+			joystick.Initialise(input);
+			swipeInput.Initialise(input);
+		}
+
+		private void Update()
+		{
+			swipeInput.Process();
+		}
+	}
+}
